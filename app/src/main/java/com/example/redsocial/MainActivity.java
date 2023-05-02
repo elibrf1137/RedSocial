2e94302c1d730cafd379ec6119c22b7093059bf8
elias.brea <elias.brea1137@gmail.com>
2023-05-02 13:24:47 +0200

    VerificacionGoogle

--- app/src/main/java/com/example/redsocial/MainActivity.java
+++ app/src/main/java/com/example/redsocial/MainActivity.java
@@ -13,10 +13,11 @@
  import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
  import com.google.android.gms.common.ConnectionResult;
  import com.google.android.gms.common.SignInButton;
+ import com.google.firebase.auth.FirebaseUser;
  import com.google.firebase.database.DatabaseReference;
  import com.google.firebase.database.FirebaseDatabase;
 
- public class MainActivity extends AppCompatActivity implements OnConnectionFailedListener {
+ public class MainActivity extends AppCompatActivity {
 
     private SignInButton googleSignInImage;
     private GoogleApiClient googleApiClient;
@@ -37,19 +38,17 @@
      private void initComponents(){
          database = FirebaseDatabase.getInstance();
          usersRef = database.getReference("Usuarios");
-         googleApiClient = new GoogleApiClient.Builder(this).
-                 enableAutoManage(this,this).
-                 addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
 
-         googleSignInImage = findViewById(R.id.imageView);
-         googleSignInImage.setSize(SignInButton.SIZE_STANDARD);
-         googleSignInImage.setColorScheme(SignInButton.COLOR_LIGHT);//Vuelve el boton transparentre
-         googleSignInImage.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 signIn();
-             }
-         });
+//         googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+//         googleSignInImage = findViewById(R.id.imageView);
+//         googleSignInImage.setSize(SignInButton.SIZE_STANDARD);
+//         googleSignInImage.setColorScheme(SignInButton.COLOR_LIGHT);//Vuelve el boton transparentre
+//         googleSignInImage.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                 signIn();
+//             }
+//         });
 
      }
 
@@ -58,32 +57,47 @@
          startActivity(crearCuenta);
      }
 
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
+     public void iniciarSesion(){
 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        // Handle connection failure
-    }
+     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
-            GoogleSignInAccount account = result.getSignInAccount();
-            // Handle successful login here
-        } else {
-            // Handle error login here
-        }
-    }
+//    private void signIn() {
+//        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(ConnectionResult connectionResult) {
+//        // Handle connection failure
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == RC_SIGN_IN) {
+//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+//            handleSignInResult(result);
+//        }
+//    }
+//
+//     private void handleSignInResult(GoogleSignInResult result) {
+//         if (result.isSuccess()) {
+//             GoogleSignInAccount account = result.getSignInAccount();
+//             AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+//             database.getInstance().signInWithCredential(credential)
+//                     .addOnCompleteListener(this, task -> {
+//                         if (task.isSuccessful()) {
+//                             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//                             String userId = user.getUid();
+//                             DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+//                             usersRef.child(userId).setValue(account);
+//                         } else {
+//                             // Handle error authentication here
+//                         }
+//                     });
+//         } else {
+//             // Handle error login here
+//         }
+//     }
 }
