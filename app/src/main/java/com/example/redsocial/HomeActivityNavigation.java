076088e62a972a816d37114570620347f48f3373
elibrf1137 <elias.brea1137@gmail.com>
2023-05-14 19:28:03 +0200

    Red social Botton Navigation View 1.0

--- app/src/main/java/com/example/redsocial/HomeActivityNavigation.java
+++ app/src/main/java/com/example/redsocial/HomeActivityNavigation.java
@@ -1,14 +1,58 @@
 package com.example.redsocial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivityNavigation extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+
+    ProfileFragment profileFragment;
+    HomeFragment homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_navigation2);
+        setContentView(R.layout.activity_home_navigation);
+        initComponente();
+        setFragment(homeFragment);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        setFragment(profileFragment);
+                        return true;
+                    case R.id.navigation_profile:
+                        setFragment(homeFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initComponente() {
+        bottomNavigationView = findViewById(R.id.bottonNavigationView);
+        frameLayout = findViewById(R.id.frametLayout);
+
+        profileFragment = new ProfileFragment();
+        homeFragment = new HomeFragment();
+
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frametLayout,fragment);
+        transaction.commit();
     }
 }
