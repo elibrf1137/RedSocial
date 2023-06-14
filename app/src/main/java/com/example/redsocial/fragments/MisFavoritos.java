e3bca494204be33db31c072735569a83446b9cab
elibrf1137 <elias.brea1137@gmail.com>
2023-06-14 20:28:55 +0200

    Esto funciona, pero hay que arreglar cositas de diseño

---
+++ app/src/main/java/com/example/redsocial/fragments/MisFavoritos.java
@@ -0,0 +1,105 @@
+package com.example.redsocial.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.redsocial.HomeActivityNavigation;
+import com.example.redsocial.R;
+import com.example.redsocial.publicaciones.AdaptadorPublicaciones;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MisFavoritos extends Fragment {
+    private String correoUser;
+    private FirebaseFirestore databaseReference;
+    private Button addPublicationButton;
+    private HashMap<String, String> listaPublicaciones;
+    private RecyclerView recyclerViewPublicaciones;
+    private AdaptadorPublicaciones adaptadorPublicaciones;
+    private View miView;
+
+    public MisFavoritos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        miView = inflater.inflate(R.layout.fragment_mis_favoritos, container, false);
+        recyclerViewPublicaciones = miView.findViewById(R.id.listaPublicacionesPersonales);
+        initComponents();
+        //Toast.makeText(getContext(),correoUser,Toast.LENGTH_SHORT).show();
+        consultaDatos();
+
+        addPublicationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                // Configura una transición de entrada y salida para los fragmentos
+                transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out);
+                // Crea una instancia del fragmento secundario y lo muestra encima del fragmento actual
+                transaction.add(android.R.id.content, new PulicationFragment()).addToBackStack(null).commit();
+
+            }
+        });
+        return miView;
+    }
+
+    private void initComponents() {
+        listaPublicaciones = new HashMap<>();
+        correoUser = HomeActivityNavigation.getCorreoUsuario();
+        addPublicationButton = miView.findViewById(R.id.addPublicationButtonProfile);
+        databaseReference = FirebaseFirestore.getInstance();
+    }
+
+    private void consultaDatos() {
+        DocumentReference documentoRef = databaseReference.collection("Users").document(correoUser);
+        // Obtén el documento
+        documentoRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists() && document.get("Favoritos") != null) {
+                        // Obtiene el valor del campo de tipo array
+                        ArrayList<String> publicaciones = (ArrayList<String>) document.get("Favoritos");
+                        if (publicaciones.size() > 0) {
+                            for (int i = 0; i < publicaciones.size(); i++) {
+                                listaPublicaciones.put(correoUser+(i), publicaciones.get(i));
+                            }
+                            mostrarDatos();
+                        }
+                    } else {
+                        Log.d("TAG", "El documento no existe");
+                    }
+                } else {
+                    Log.d("TAG", "Error obteniendo el documento: ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void mostrarDatos() {
+        recyclerViewPublicaciones.setLayoutManager(new LinearLayoutManager(getContext()));
+        adaptadorPublicaciones = new AdaptadorPublicaciones(miView.getContext(), listaPublicaciones);
+        recyclerViewPublicaciones.setAdapter(adaptadorPublicaciones);
+    }
+}
--- app/src/main/java/com/example/redsocial/publicaciones/AdaptadorPublicaciones.java
+++ app/src/main/java/com/example/redsocial/publicaciones/AdaptadorPublicaciones.java
@@ -6,27 +6,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.redsocial.HomeActivityNavigation;
 import com.example.redsocial.R;
-import com.example.redsocial.fragments.HomeFragment;
-import com.example.redsocial.fragments.ProfileFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class AdaptadorPublicaciones  extends RecyclerView.Adapter<AdaptadorPublicaciones.ViewHolder> implements View.OnClickListener{
 
     LayoutInflater layoutInflater;
     HashMap<String, String> listaPublicacion;
+    Button corazonButton;
     View.OnClickListener onClickListener;
+    FirebaseFirestore miBaseDatos;
+    String correoUser = HomeActivityNavigation.getCorreoUsuario();
+
 
 
     public AdaptadorPublicaciones (Context context,HashMap listaPublicacion){
@@ -60,6 +66,39 @@ public class AdaptadorPublicaciones  extends RecyclerView.Adapter<AdaptadorPubli
         holder.contenido.setText(contenidoPublicacion);
         holder.contenido.setEnabled(false);
 
+        corazonButton = holder.corazon;
+        corazonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                agregarPublicaciones(contenidoPublicacion);
+            }
+        });
+
+    }
+
+    public void agregarPublicaciones(String publicacion){
+
+        // Obtén una referencia al documento
+
+        miBaseDatos = FirebaseFirestore.getInstance();
+        DocumentReference docRefUser = miBaseDatos.collection("Users").document(correoUser);
+        CollectionReference collectionRefPublication = miBaseDatos.collection("Favoritos");
+
+        // Actualiza el array en el documento
+        docRefUser.update("Favoritos", FieldValue.arrayUnion(publicacion)).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("Firestore", "Elemento añadido al array correctamente");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Firestore", "Error al añadir el elemento al array: " + e.getMessage());
+            }
+        });
+        Map<String,Object> publicacionAnhadida = new HashMap<>();
+        publicacionAnhadida.put(correoUser+publicacionAnhadida,publicacion);
+        collectionRefPublication.add(publicacionAnhadida);
     }
 
     @Override
@@ -76,11 +115,11 @@ public class AdaptadorPublicaciones  extends RecyclerView.Adapter<AdaptadorPubli
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView contenido;
-        ImageView corazonRojo;
-        ImageView corazonNegro;
+        Button corazon;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             contenido = itemView.findViewById(R.id.descTextView);
+            corazon = itemView.findViewById(R.id.corazonButton);
         }
     }
 }
